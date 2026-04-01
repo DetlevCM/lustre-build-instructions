dff5117323a791f43fb1b3b2879badc5a07a9ccf
Detlev Conrad Mielczarek <detlev.conrad.mielczarek@hu-berlin.de>
2026-04-01 12:10:53 +0200

    also build the dkms packages for rocky

--- build_in_docker/lustre_builder_rocky_9/build/build-rocky.bash
+++ build_in_docker/lustre_builder_rocky_9/build/build-rocky.bash
@@ -180,11 +180,19 @@ else
 ./configure --with-linux=/usr/src/kernels/$linuxVersion
 fi
 
+## make regular rpms
 make rpms
+mkdir -p $Buildpath-rpm
+mv ./*.rpm $Buildpath-rpm/
+
+## make dkms rpms
+make dkms-rpm
+mkdir -p $Buildpath-dkms-rpm
+mv ./*.rpm $Buildpath-dkms-rpm/
 
 # make the folder read- and writable to all
 chmod -R a+rw $Buildpath
+chmod -R a+rw $Buildpath-rpm
+chmod -R a+rw $Buildpath-dkms-rpm
 
-mkdir -p $Buildpath-rpm
-mv ./*.rpm $Buildpath-rpm/
 
