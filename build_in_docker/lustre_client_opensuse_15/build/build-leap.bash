bab9691219701d5829c3524c065d6b69115f5e08
Detlev Conrad Mielczarek <detlev.conrad.mielczarek@hu-berlin.de>
2026-04-16 15:37:44 +0200

    updates to the leap builder, Dockerfile and Script

--- build_in_docker/lustre_client_opensuse_15/build/build-leap.bash
+++ build_in_docker/lustre_client_opensuse_15/build/build-leap.bash
@@ -1,5 +1,60 @@
 #/bin/bash
 
+## default variables for user parameters
+versionKernelMacros=""
+versionKernelSyms=""
+versionKernel=""
+BuildServer="false"
+versionE2fsck="v1.47.3-wc1" # known good version for lustre 2.17.0
+versionLustre="master"
+
+versionLinux=""
+noarchVersion=""
+
+##
+## set whether lustre sources are already provided
+## set the version og the lustre client to build
+## set a specific linux kernel package to use if required
+##
+LustreSourceRepo=/build/lustre-release.src
+
+## e2fsprogs repo only needed with server build
+e2fsprogsSourceRepo=/build/e2fsprogs.src
+
+
+function split_arguments() {
+  echo "$1" | cut -d "=" -f 2
+}
+
+## process user arguments
+for arg; do
+
+echo $arg
+
+if [ $arg == "server" ]  ; then
+#BuildServer="true"
+echo "unfortunately building the server is not currently supported on Leap by this script"
+fi
+
+if [[ $arg == --e2fsckVersion=* ]] ; then
+  versionE2fsck=$(split_arguments $arg)
+  echo $versionE2fsck
+fi
+
+if [[ $arg == --versionLustre=* ]] ; then
+  versionLustre=$(split_arguments $arg)
+  echo $versionLustre
+fi
+
+if [[ $arg == --versionLinux=* ]] ; then
+  versionLinux="linux-"$(split_arguments $arg)
+  echo $versionLinux
+fi
+
+
+done
+
+
 
 ##
 ## set whether lustre sources are already provided
@@ -9,35 +64,57 @@
 
 #### NOTE: Client only
 #### will use the latest kernel available in the container
+#### or accepts user supplied rpms
 
 
-LustreSourceRepo=/build/lustre-release.bak
-Version="2.17.0"
-linuxVersion=""
-## set kernel-devel manually
-#linuxVersion="linux-6.4.0-150600.23.87"
+## Current kernels can be found in the opensuse repositories, however there is no history:
+## paths are included as a reference for potential future developments 
+## there also seems to be version number mismatches in the repo - but not when insatlling via zypper
+## https://download.opensuse.org/update/leap/15.6/sle/noarch/kernel-macros-6.4.0-150600.23.7.2.noarch.rpm
+## https://download.opensuse.org/distribution/leap/15.6/repo/oss/x86_64/kernel-syms-6.4.0-150600.16.1.x86_64.rpm
+## https://download.opensuse.org/distribution/leap/15.6/repo/oss/x86_64/kernel-default-6.4.0-150600.16.3.x86_64.rpm
+## https://download.opensuse.org/distribution/leap/15.6/repo/oss/x86_64/kernel-default-devel-6.4.0-150600.16.3.x86_64.rpm
+## https://download.opensuse.org/update/leap/15.6/sle/noarch/kernel-devel-6.4.0-150600.23.7.2.noarch.rpm
 
 
-## fails to install via Dockerfile...
-## so these need to be installed via the bash script
+## if the versionLinux is blank:
+if [ -z $versionLinux ] ; then
+
+## install the latest packages needed to build lustre
 zypper install -y kernel-syms kernel-macros kernel-default kernel-default-devel
+## get the rpms for safekeeping, kernel-devel is really optional, but might just be good to keep
+zypper install -y --force --download-only --no-recommends kernel-syms kernel-macros kernel-default kernel-default-devel kernel-devel
 
 ## get the current linux version in the container in a very hacky way...
-linuxVersion=$(ls /usr/src/linux-* | cut -c 10- | head -1)
+versionLinux=$(ls /usr/src/linux-* | cut -c 10- | head -1)
 # remove trailing colon
-linuxVersion=${linuxVersion::-1}
+versionLinux=${versionLinux::-1}
+
+#echo "FROM REPO"
+## save the kernel packages so they are available
+mkdir -p /build/$versionLinux
+mv $(find /var/cache/zypp/packages/ -name 'kernel-default*') /build/$versionLinux
+mv $(find /var/cache/zypp/packages/ -name 'kernel-syms*') /build/$versionLinux
+mv $(find /var/cache/zypp/packages/ -name 'kernel-macros*') /build/$versionLinux
+mv $(find /var/cache/zypp/packages/ -name 'kernel-devel*') /build/$versionLinux
+
+elif [ -d /build/$versionLinux ] ; then
+#echo "FROM DIR"
+## move to the directory with the rpms and install them
+## we only use the path of the directory - one directory per kernel release
+## version number MUST match, as it is used later on for the lustre paths to object files, etc.
+cd /build/$versionLinux
+zypper install -y --force --no-recommends ./kernel-syms-*.rpm ./kernel-macros-*.rpm ./kernel-default-*.rpm ./kernel-default-devel-*.rpm
+cd /build
+
+else
+## not the current kernel, no packages provided, so exit
+echo "ERROR, current kernel undesired, required packages not supplied"
+exit ; echo $1
+fi
 
 ## e2fsprogs repo only needed with server build
 e2fsprogsSourceRepo=/build/e2fsprogs.bak
-noarchVersion=""
-
-
-## if a Linux version is set, we also need to be able to define noarch
-if [ -n $linuxVersion ];
-then
-## written for modern bash
-noarchVersion=${linuxVersion::-6}noarch
-fi
 
 ##
 ## Now start the scripted process
@@ -56,22 +133,22 @@ if [ "$1" == "server" ];
 then
 if [ ! -d "$e2fsprogsSourceRepo" ];
 then
-git clone "https://review.whamcloud.com/tools/e2fsprogs"
+git clone "https://review.whamcloud.com/tools/e2fsprogs" $e2fsprogsSourceRepo
 fi
 fi
 
 
 ## build and install e2fsprogs if we want to build the server
-if [ "$1" == "server" ];
+if [ "$BuildServer" == "true" ];
 then
 
-Buildpath=/build/e2fsprogs-$Version-$linuxVersion
+Buildpath=/build/e2fsprogs-$versionLustre-$versionLinux
 echo $Buildpath
-cp -r $e2fsprogsSourceRepo /build/e2fsprogs-$Version-$linuxVersion
+cp -r $e2fsprogsSourceRepo /build/e2fsprogs-$versionLustre-$versionLinux
 cd $Buildpath
 
 ## build e2fsprogs
-git checkout v1.47.3-wc1 # tested
+git checkout $versionE2fsck
 ./configure --enable-elf-shlibs
 
 ## these two tests fail
@@ -90,17 +167,31 @@ fi
 
 
 
-Buildpath=/build/lustre-release-$Version-$linuxVersion
-echo $Buildpath
-cp -r $LustreSourceRepo /build/lustre-release-$Version-$linuxVersion
+Buildpath=/build/lustre-release-$versionLustre-$versionLinux
+#echo $versionLinux
+#echo $LustreSourceRepo
+#echo $Buildpath
+cp -r $LustreSourceRepo /build/lustre-release-$versionLustre-$versionLinux
+
 cd $Buildpath
+echo $(pwd)
 
-git checkout $Version
+git checkout $versionLustre
 
+## disable the kernel check - we build in a container
+sed -i 's/BuildRequires: kernel >= 3.10/#BuildRequires: kernel >= 3.10/g' $Buildpath/lustre.spec.in
 
 ./autogen.sh
+#if [ "$BuildServer" == "true" ];
+#then
+## for the server support build we need to disable another check
+#sed -i 's/! grep -q define\[\[\:space\:\]\]\*HAVE_SERVER_SUPPORT config.h 2> \/dev\/null/false/g'  $Buildpath/lustre.spec.in
+#./configure --enable-server --with-linux=/usr/src/kernels/$versionLinux
+#else
+## currently supports only client builds
+./configure --with-linux=/usr/src/$versionLinux --with-linux-obj=/usr/src/$versionLinux-obj/x86_64/default
+#fi
 
-./configure --with-linux=/usr/src/$linuxVersion --with-linux-obj=/usr/src/$linuxVersion-obj/x86_64/default
 
 ## make regular rpms
 make rpms
